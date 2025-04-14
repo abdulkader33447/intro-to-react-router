663df9b1cad010fb4bc5fa51bde55d9e5f9931f9
abdulkader33447 <abdulkader33447@gmail.com>
2025-04-15 00:49:28 +0600

    added userdetail2

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { Suspense, useState } from "react";
+import { Link } from "react-router";
+import UserDetail2 from "../UserDetail2/UserDetail2";
 
-const User = ({user}) => {
-const {id,name, email,phone}=user;
+const User = ({ user }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  const { id, name, email, phone } = user;
 
-const userStyle ={
-  border:'2px solid green',
-  borderRadius:'20px',
-  padding:'10px',
-  margin:'10px'
-}
+  const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+  const userStyle = {
+    border: "2px solid green",
+    borderRadius: "20px",
+    padding: "10px",
+    margin: "10px",
+  };
 
   return (
     <div style={userStyle}>
       <h3>Name : {name}</h3>
-      <p>email : <small>{email}</small></p>
-      <p>Phone : <small>{phone}</small></p>
+      <p>
+        email : <small>{email}</small>
+      </p>
+      <p>
+        Phone : <small>{phone}</small>
+      </p>
       <Link to={`/users/${id}`}>Show detail</Link>
+
+      {/* " ! " toggle er kaj kore */}
+      <button onClick={() => setShowInfo(!showInfo)}>
+        {showInfo ? "Hide" : "Show"} info
+      </button>
+      {showInfo && (
+        <Suspense fallback={<span>Loading....</span>}>
+          <UserDetail2 userPromise={userPromise}></UserDetail2>
+        </Suspense>
+      )}
     </div>
   );
 };
---
+++ src/components/UserDetail2/UserDetail2.jsx
@@ -0,0 +1,13 @@
+import React, { use } from 'react';
+
+const UserDetail2 = ({userPromise}) => {
+const {name,userNam} = use(userPromise)
+
+  return (
+    <div>
+      <p><small></small></p>
+    </div>
+  );
+};
+
+export default UserDetail2;
